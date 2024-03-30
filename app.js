a4a1b9be26197881b877e699ddf5fbc8f083825b
DK <128988989+dkk934@users.noreply.github.com>
2024-03-30 21:54:03 +0530

    Update app.js
    change port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import bodyParser from "body-parser";
 import pg from "pg";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const db = new pg.Client({
   user: "postgres",
